a4b49159298c40f5317c3370d7621b58552d798f
Anthony Garza <agarza1972@yahoo.com>
2014-05-30 16:02:33 -0500

    Updated the TODO list by adding a form to allow a file to be uploaded. When a file is uploaded, move it to the uploads directory (see example in this lesson) and process the file, adding new todos to your existing todo list.

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html>
+
+	<?php
+		var_dump($_GET);
+		var_dump($_POST);
+    ?>
+
 <head>
 </head>
 	<title>TODO List</title>
@@ -9,9 +15,9 @@
 	$filename = 'data/list.txt';
 	$items = array();
 
-	function open_file()
+	function open_file($filename = 'data/list.txt')
 	{
-	    $filename = 'data/list.txt';
+	    // $filename = 'data/list.txt';
 	    $filesize = filesize($filename);
 	    $read = fopen($filename, 'r');
 	    $list_string = trim(fread($read, $filesize));
@@ -28,12 +34,37 @@
 	    fwrite($write, $string . "\n");
 	    fclose($write);
 	}
+
+	$items = open_file($filename);
+	// Verify there were uploaded files and no errors
+	if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+	    // Set the destination directory for uploads
+	    $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+	    // Grab the filename from the uploaded file by using basename
+	    $filename = basename($_FILES['file1']['name']);
+	    // Create the saved filename using the file's original name and our upload directory
+	    $saved_filename = $upload_dir . $filename;
+	    // Move the file from the temp location to our uploads directory
+	    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+
+	    // upload file to existing TODO list
+	    $uploadedItems = open_file($saved_filename);
+	    // var_dump($uploadedItems);
+		$items = array_merge($items, $uploadedItems);
+		// var_dump($items);
+	}
+
+	// Check if we saved a file
+	if (isset($saved_filename)) {
+	    // If we did, show a link to the uploaded file
+	    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+	}
+	save_file($items, $filename);
+
 	?>
+
 <body>
-	<?php
-		var_dump($_GET);
-		var_dump($_POST);
-    ?>
+
 	<h1>TODO List</h1>
 	<ul>
 		<?php
@@ -73,5 +104,19 @@
 			<input type="submit" value="Submit">
 		</p>
 	</form>
+
+
+	<h1>Upload File</h1>
+
+	<form method="POST" enctype="multipart/form-data">
+	    <p>
+	        <label for="file1">File to upload: </label>
+	        <input type="file" id="file1" name="file1">
+	    </p>
+	    <p>
+	        <input type="submit" value="Upload">
+	    </p>
+	</form>
+
 </body>
 </html>
