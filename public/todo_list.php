c410adffee0131a68a8549f24e1138cd152855ce
Anthony Garza <agarza1972@yahoo.com>
2014-06-19 11:34:28 -0500

    Refactored code to require filestore class that reads and writes to a file

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,128 +1,104 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-	<?
-		var_dump($_GET);
-		var_dump($_POST);
-    ?>
+require_once('classes/filestore.php');
+// var_dump($_GET);
+// var_dump($_POST);
 
+//variables
+$todos = [];  //array to hold todo items
+
+$error_msg=''; //initailize variable to hold error messages
+
+$todo_store = new Filestore('data/list.txt');
+
+
+//go thru the local text file and add to the array
+$file_items = $todo_store->read_lines();
+
+if ($file_items !== FALSE){
+    foreach ($file_items as $list_item) {
+        array_push($todos, $list_item); //add to the end of the array
+    } //end of foreach
+} // end of if
+
+//add new todo items from POST
+if (!empty($_POST['task'])){
+	$newTodo = $_POST['task']; //assign the variable from the post
+	$todos[] = $newTodo; // add to the array
+	$todo_store->write_lines($todos); // save your file
+	header('Location: /todo_List.php');
+	exit(0);
+} //end of add item
+
+//remove item from todo array using GET
+if (isset($_GET['remove_item']) ){
+	 $removeItem = $_GET['remove_item'];
+	 unset($todos[$removeItem]); //remove from todo array
+	 $todo_store->write_lines($todos); // save your file
+	 header('Location: /todo_List.php');
+	 exit(0);
+} //end of remove item
+
+//move uploaded files to the upload directory
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+	if ($_FILES['file1']['type'] == 'text/plain'){
+		$upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+	    // Grab the filename from the uploaded file by using basename
+	    $filename = basename($_FILES['file1']['name']);
+	    // Create the saved filename using the file's original name and our upload directory
+	    $saved_filename = $upload_dir . $filename;
+	    // Move the file from the temp location to our uploads directory
+	    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+
+	    //add items to the todo list
+	    $saved_file_items = $todo_store->read_lines($saved_filename);
+	    foreach ($saved_file_items as $list_item) {
+	        array_push($todos, $list_item); //add to the end of the array
+	    } //end of foreach
+	    $todo_store->write_lines($todos); // save your file
+	} // Set the destination directory for uploads
+    else{
+    	$error_msg = 'Upload error: wrong file type. Must be .txt';
+    }
+} //end of if to move uploaded files
+?>
+
+<!doctype html>
+<html lang="en">
 <head>
-</head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/sites.css">
 	<title>TODO List</title>
-	<link rel="stylesheet" href="/css/site.css">
-
-	<?
-	define('FILENAME', 'data/todo.txt');
-
-	$filename = 'data/list.txt';
-	$items = array();
-
-	function open_file($filename = 'data/list.txt')
-	{
-	    // $filename = 'data/list.txt';
-	    $filesize = filesize($filename);
-	    $read = fopen($filename, 'r');
-	    $list_string = trim(fread($read, $filesize));
-	    $list = explode("\n", $list_string);
-	    return $list;
-	    fclose($read);
-	}
-
-	function save_file($list, $filename)
-	{
-		$filename = 'data/list.txt';
-	    $write = fopen($filename, 'w');
-	    $string = implode("\n", $list);
-	    fwrite($write, $string . "\n");
-	    fclose($write);
-	}
-
-	$items = open_file($filename);
-	// Verify there were uploaded files and no errors
-	if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
-		if ($_FILES['file1']['type'] == 'text/plain') {
-		    // Set the destination directory for uploads
-		    $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
-		    // Grab the filename from the uploaded file by using basename
-		    $filename = basename($_FILES['file1']['name']);
-		    // Create the saved filename using the file's original name and our upload directory
-		    $saved_filename = $upload_dir . $filename;
-		    // Move the file from the temp location to our uploads directory
-		    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
-
-		    // upload file to existing TODO list
-		    $uploadedItems = open_file($saved_filename);
-		    // var_dump($uploadedItems);
-			$items = array_merge($items, $uploadedItems);
-			// var_dump($items);
-		} else {
-			echo "Please upload .txt file only!";
-		}
-	}
-
-	// Check if we saved a file
-	if (isset($saved_filename)) {
-	    // If we did, show a link to the uploaded file
-	    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
-	}
-	save_file($items, $filename);
-
-	?>
-
+</head>
 <body>
-
 	<h1>TODO List</h1>
+	<? if(!empty($error_msg)) : ?>
+		<?= PHP_EOL . $error_msg . PHP_EOL;?>
+	<? endif; ?>
+
+
+	<!-- output array on screen -->
 	<ul>
-		<?
-		// loads TODO list items from a file
-		$items = open_file();
-
-        // Add TODO items to list and Save to file
-        if (!empty($_POST['input_item'])) :
-        	array_push($items, htmlspecialchars(strip_tags($_POST['input_item'])));
-
-
-    	// removes link to TODO item thet user clicks on to remove
-    	elseif (isset($_GET['removeIndex'])) :
-    		$removeIndex = $_GET['removeIndex'];
-    		unset($items[$removeIndex]);
-    	endif;
-
-    	// displays items and provides indexed link to item
-	    foreach ($items as $index => $item) : ?>
-		<?= "<li>$item <a href=\"todo_list.php?removeIndex={$index}\">Remove item</a></li>\n"; ?>
-	    <? endforeach;
-
-	    // saves TODO list to file
-	    save_file($items, $filename);
-
-	    ?>
+		<? foreach ($todos as $key => $todo) :?>
+		<!-- sanitize user input -->
+		<? $todo = htmlspecialchars(strip_tags($todo)); ?>
+		<?= "<li>$todo <a href=\"http://todo.dev/todo_List.php?remove_item=$key\">Remove Item</a></li>\n"; ?>
+		<? endforeach; ?>
 	</ul>
 
-	<h2>Enter TODO List Items to Add Below</h2>
-
-	<form method="POST">
-		<p>
-		    <label for="input_item">ENTER TODO LIST ITEM TO ADD:</label>
-	        <input id="input_item" name="input_item" type="text" placeholder="Input Item">
-		</p>
-		<p>
-			<input type="submit" value="Submit">
-		</p>
+	<h2>Input New Todo Items</h2>
+	<form method="POST" action="/todo_List.php">
+        <label for="task">Task</label>
+        <input id="task" name="task" type="text" placeholder="Add Todo Item">
+        <button type="submit">Add Item</button>
 	</form>
-
 
 	<h2>Upload File</h2>
-
 	<form method="POST" enctype="multipart/form-data">
-	    <p>
-	        <label for="file1">File to upload: </label>
-	        <input type="file" id="file1" name="file1">
-	    </p>
-	    <p>
-	        <input type="submit" value="Upload">
-	    </p>
+	    <label for="file1">File to upload: </label>
+	    <input type="file" id="file1" name="file1">
+		<br>
+	    <input type="submit" value="Upload">
 	</form>
-
 </body>
 </html>
